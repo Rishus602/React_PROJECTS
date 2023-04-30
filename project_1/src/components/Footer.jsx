8f9462d24dd376faa2051f81330a6b787f9e8d5a
Rishus602 <rishabhsahni2001@gmail.com>
2023-04-30 21:44:48 +0530

    another commit

---
+++ project_1/src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import "../styles/footer.scss"
+
+const Footer = () => {
+  return (
+
+    <footer>
+    <div>
+      <h1>
+      Rishabh's Tech
+</h1>
+<p>@all right reserved</p>
+
+
+    </div>
+    <div>
+    <h5>Follow Us</h5>
+    <div>
+      <a href="https://www.linkedin.com/in/rishabhsahni27/" target={"blank"}>
+        LinkedIn
+      </a>
+      <a href="https://www.instagram.com/the_rishabh_sahni/" target={"blank"}>
+        Instagram
+      </a>
+      <a href="https://github.com/Rishus602" target={"blank"}>
+        Github
+      </a>
+    </div>
+  </div>
+  </footer>
+  )
+}
+
+export default Footer
